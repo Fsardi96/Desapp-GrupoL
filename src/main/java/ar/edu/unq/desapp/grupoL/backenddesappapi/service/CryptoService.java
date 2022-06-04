809f1faeb0ac3e5b418006d3fce82ec0459f6565
Facundo Sardi <facusardi96@gmail.com>
2022-06-04 19:07:28 -0300

    Se implementa endpoint que trae el dolar del día de hoy, Revisar createTransaction que rompe en la 2da transaction creada

--- src/main/java/ar/edu/unq/desapp/grupoL/backenddesappapi/service/CryptoService.java
+++ src/main/java/ar/edu/unq/desapp/grupoL/backenddesappapi/service/CryptoService.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -28,4 +31,22 @@ public class CryptoService {
         }
         return entity;
     }
+
+    public static String getResponseBody(HttpURLConnection conn) {
+        BufferedReader br = null;
+        StringBuilder body = null;
+        String line = "";
+        try {//  w  w w .j  a  v a 2s . c  o m
+            br = new BufferedReader(new InputStreamReader(
+                    conn.getInputStream()));
+            body = new StringBuilder();
+            while ((line = br.readLine()) != null)
+                body.append(line);
+            return body.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
--- src/main/java/ar/edu/unq/desapp/grupoL/backenddesappapi/webservice/CryptoController.java
+++ src/main/java/ar/edu/unq/desapp/grupoL/backenddesappapi/webservice/CryptoController.java
@@ -9,6 +9,8 @@ import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +20,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -30,6 +37,11 @@ public class CryptoController {
     @Autowired
     CryptoService cryptoService;
     private RestTemplate restTemplate = new RestTemplate();
+
+
+
+
+
 
     @Operation(summary = "Get a cryptocurrency price")
     @GetMapping("/getCrypoValue/{cryptoSymbol}")
@@ -52,5 +64,19 @@ public class CryptoController {
         }
         return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping("/api/usd_price")
+    public ResponseEntity<String> getUSDPrice() throws IOException {
+        URL url = new URL("https://api.estadisticasbcra.com/usd_of");
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestProperty("Accept", "application/json");
+        http.setRequestProperty("Authorization", "BEARER eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODE4Njc5NzUsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJmYWN1c2FyZGk5NkBnbWFpbC5jb20ifQ.HC2l0NbZ6vdZLEEOUkkZnSy7Ce8x3nbvg2m7nLzdmERmTyKphnVK1v8SE7g-VzYohHc97hyDXwkQViq6w0gasg");
+        String response = CryptoService.getResponseBody(http);
+        JSONArray array = new JSONArray(response);
+        JSONObject json = new JSONObject();
+        json.put("usd_prices", array.get(array.length() -1));
+        return ResponseEntity.ok().body(json.toString());
+    }
+
 
 }
